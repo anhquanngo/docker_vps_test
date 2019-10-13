dc0e32953dfe77a10ce4a3f860a194d2c275dae6
Quan Anh <43566591+anhquanngo@users.noreply.github.com>
2019-10-14 02:02:07 +0700

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ const port = process.env.PORT || 3000
 app.get('/get-server-name', (req, res) => {
     res.send('Hello world')
     res.send(`Hi from ${process.env.SERVER_NAME}`)
-    res.send(JSON.stringify(process.env));
-    console.log(process.env.SERVER_NAME) 
 })
 app.listen(port, () => {
     console.log('connect to ' + port);
